ffa372643bf825d54667bf5ba4b6a34b392b85fe
SiegmannGiS <marcel.siegmann@uibk.ac.at>
2018-09-12 15:32:17 +0200

    diagonal movement

--- session1/first_try.py
+++ session1/first_try.py
@@ -18,7 +18,18 @@ def move(start, move_string):
         elif direction == "w":
             plt.plot([cur_pos[0], cur_pos[0] - int(step) * int(blocks)], [cur_pos[1], cur_pos[1]], 'r-o')
             cur_pos = (cur_pos[0] - int(step) * int(blocks), cur_pos[1])
-
+        elif direction == "ne":
+            plt.plot([cur_pos[0], cur_pos[0] + int(step) * int(blocks)], [cur_pos[1], cur_pos[1] - int(step) * int(blocks)], 'r-o')
+            cur_pos = (cur_pos[0] + int(step) * int(blocks), cur_pos[1] - int(step) * int(blocks))
+        elif direction == "se":
+            plt.plot([cur_pos[0], cur_pos[0] + int(step) * int(blocks)], [cur_pos[1], cur_pos[1] + int(step) * int(blocks)], 'r-o')
+            cur_pos = (cur_pos[0] + int(step) * int(blocks), cur_pos[1] + int(step) * int(blocks))
+        elif direction == "sw":
+            plt.plot([cur_pos[0], cur_pos[0] - int(step) * int(blocks)], [cur_pos[1], cur_pos[1] + int(step) * int(blocks)], 'r-o')
+            cur_pos = (cur_pos[0] - int(step) * int(blocks), cur_pos[1] + int(step) * int(blocks))
+        elif direction == "nw":
+            plt.plot([cur_pos[0], cur_pos[0] - int(step) * int(blocks)], [cur_pos[1], cur_pos[1] - int(step) * int(blocks)], 'r-o')
+            cur_pos = (cur_pos[0] - int(step) * int(blocks), cur_pos[1] - int(step) * int(blocks))
 
 # create window
 window = np.zeros((40, 20))
@@ -38,7 +49,7 @@ np.savetxt(r"C:\Users\Marcel\PycharmProjects\untitled5\session1\test.txt", windo
 plt.imshow(window, cmap="seismic")
 # Robot movement
 start = [10, 36]
-movement_string = ["6_n_3", "1_e_3", "1_n_3", "1_e_3", "2_n_3", "1_w_3", "1_w_1"]
+movement_string = ["6_n_3", "1_e_3", "1_n_3", "1_e_3", "2_n_3", "1_w_3", "1_w_1", "1_ne_3"]
 move(start, movement_string)
 
 plt.show()
